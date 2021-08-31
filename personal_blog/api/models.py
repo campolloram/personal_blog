6eda0d9299b2495cb0e896ac0cf767555af3218c
Guillermo <campollo.ram@gmail.com>
2021-08-30 22:52:59 -0500

    Added logic for saving posts with same title, added gitignore

--- personal_blog/api/models.py
+++ personal_blog/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.db import IntegrityError
 
 
 
@@ -12,9 +14,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    title = models.CharField(null=False, max_length=120, unique=True)
+    title = models.CharField(null=False, max_length=120)
     body = models.TextField(null=False)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, null=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts', default=1)
     
 
@@ -24,6 +26,24 @@ class Post(models.Model):
         verbose_name_plural = 'posts'
     
     def save(self, *args, **kwargs):
-        if not self.pk:
-            self.slug = '_'.join([word for word in self.title.split(' ')])
-            self.status = 'Publish'
+        slug = slugify(self.title)
+        try:
+            if not self.pk:
+                self.slug = slug
+                self.status = 1
+                return super(Post, self).save(*args, **kwargs)
+
+        except IntegrityError:
+            c = 1
+            while True:
+                try:
+                    Post.objects.get(slug=slug+f'_{c}')
+                    c += 1
+
+                except Post.DoesNotExist:
+                    self.slug = slug + f'_{c}'
+                    self.status = 1
+                    break
+
+        finally:
+            return super(Post, self).save(*args, **kwargs)
