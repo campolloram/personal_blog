b2cd6756798729177c675e413a5ef9ab07bcbf7e
Guillermo <campollo.ram@gmail.com>
2021-12-12 19:05:29 -0600

    Stylings

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="container-menu">
+            <h1> Guillermo Campollo's Blog</h1>
+            <p className="item">Hey everyone, my name is Guillermo Campollo and I'm a software developer. 
+                I created this webpage to showcase and practice my skills. It is created using Django for the backend and
+                React on the frontend!</p>
+        </div>
+    )
+}
+
+export default About;
